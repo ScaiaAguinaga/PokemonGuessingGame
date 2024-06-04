970ee488ad958af2c42d5a6e15c66f1bec5690e9
Isaac <Scaiaaguinaga@gmail.com>
2024-06-04 16:54:26 -0700

    Got PokeAPI working and fetching data dynamically.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
 const Home = () => {
-  return <div className="font-bold">Home</div>;
+  async function fetchData() {
+    try {
+      const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      if (!response.ok) {
+        throw new Error("Could not fetch resource");
+      }
+
+      const data = await response.json();
+
+      document.getElementById("pokemonSprite").src=data.sprites.front_default
+
+      console.log(data);
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // getPokeData();
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <img id="pokemonSprite" src="" className="h-[300px]"/>
+      <input type="text" id="pokemonName" className="mx-5 border-2 border-black p-1"></input>
+      <button onClick={() => fetchData()} className="border-2 border-black p-1">
+        Fetch Pokemon Data
+      </button>
+    </div>
+  );
 };
 
 export default Home;
